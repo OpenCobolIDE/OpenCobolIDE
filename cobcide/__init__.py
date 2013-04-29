740e5938b59e0237960d5ca3e7df975fb3d7e1d0
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2013-04-29 22:34:55 +0200

    Bump to version 1.3.0

--- cobcide/__init__.py
+++ cobcide/__init__.py
@@ -20,7 +20,7 @@ This module defines the app constants such as the version, the supported file
 types,...
 """
 #: The OpenCobol IDE version
-__version__ = "1.2.1"
+__version__ = "1.3.0"
 
 
 class FileType:
