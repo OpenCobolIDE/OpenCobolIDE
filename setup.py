4ffb579af898693206ee3c76bb70f36f78479bc4
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2013-12-08 18:39:25 +0100

    Bump version in setup.py too!

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ if sys.platform == "linux":
 
 setup(
     name='OpenCobolIDE',
-    version="2.0.1",
+    version="2.1.0",
     packages=find_packages(),
     keywords=["Cobol; OpenCobol; IDE"],
     package_data={'oci.ui': ['*.ui', 'rc/*']},
