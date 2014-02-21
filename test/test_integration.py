f9e6df4c591e15198abeedc7b62c231bf1e8676f
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2014-02-21 19:59:27 +0100

    Add test integration

---
+++ test/test_integration.py
@@ -0,0 +1,42 @@
+"""
+Integration test:
+  - start the IDE
+  - open a cobol file
+  - compile it
+  - run the generated executable
+"""
+import os
+import sys
+import time
+from pyqode.qt import QtGui, QtCore
+from oci import main_window, constants
+
+
+def setup_module(module):
+    """
+    Creates hello.cbl based on the hello world template code.
+    """
+    with open("hello.cbl", "w") as f:
+        f.write(constants.EXE_TEMPLATE)
+
+
+def teardown_module(module):
+    """
+    Deletes hello.cbl
+    """
+    os.remove("hello.cbl")
+    os.remove("bin/hello.exe")
+    os.rmdir("bin")
+
+
+def test_integration():
+    app = QtGui.QApplication(sys.argv)
+    window = main_window.MainWindow()
+    window.show()
+    window.openFile(os.path.abspath("hello.cbl"))
+    QtCore.QTimer.singleShot(
+        1000, window.on_actionCompile_triggered)
+    QtCore.QTimer.singleShot(
+        3000, window.on_actionRun_triggered)
+    QtCore.QTimer.singleShot(5000, app.quit)
+    app.exec_()
