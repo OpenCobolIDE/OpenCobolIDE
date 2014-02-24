f78a953cda4209378dc45cf82df8e4f036e81a7f
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2014-02-24 19:21:56 +0100

    Add Python-Cobol script
    from https://github.com/OpenCobolIDE/Python-COBOL

---
+++ oci/python_cobol/__init__.py
@@ -0,0 +1,13 @@
+"""
+This package contains the Python-Cobol pic parser written by
+Brian Peterson (https://github.com/bpeterso2000/pycobol) and enhanced
+by Paulus Schoutsen (https://github.com/balloob/Python-COBOL/blob/master/cobol.py)
+
+The only modifications done by the OpenCobolIDE team is to add support
+for python3 and replace print statement with call to logging module. Those
+modifications have been implemented on a fork of the Python-Cobol project,
+maintained by the OpenCobolIDE organization: https://github.com/OpenCobolIDE/Python-COBOL
+
+PLEASE, if you want to do any modification on the cobol.py script, do it
+on the fork then copy the script here!!!
+"""
