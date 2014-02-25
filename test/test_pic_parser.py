91f2f568e30b3dfe576ae50d542b4f510c383596
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2014-02-25 11:19:38 +0100

    Improve pic parser test
    (Should faild on Travis)

--- test/test_pic_parser.py
+++ test/test_pic_parser.py
@@ -3,9 +3,9 @@ Tests the pic fields parser
 """
 from oci.pic_parser import get_field_infos
 
-source="""       01 INPUT-DATA.
-           05 U1-DINRAR PIC X(06).
-           05 FILLER PIC X(01).
+source = """       01 INPUT-DATA.
+           05 U1-DINRAR PIC X(06)        .
+           05 FILLER PIC X(01)  VALUE ';'.
            05 U1-DINR PIC X(07).
            05 FILLER PIC X(01).
            05 U1-ORGNR PIC X(10).
