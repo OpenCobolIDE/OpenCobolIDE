5ad6626bee3d64b024b20852ff74c4905b07b8b2
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2014-08-27 23:37:46 +0200

    Add linter mode

---
+++ open_cobol_ide/linter.py
@@ -0,0 +1,30 @@
+"""
+Cobol linter; use open cobol to check your your syntax on the fly.
+"""
+import os
+from pyqode.core.modes import CheckerMode
+from .compiler import GnuCobolCompiler, get_file_type
+from .system import get_cache_directory
+
+
+def lint(request_data):
+    """
+    Performs linting of a cobol document.
+
+    This method will perform on the pyqode backend.
+
+    :param request_data: work request data (dict)
+    :return: status, messages
+    """
+    code = request_data['code']
+    tmp_pth = os.path.join(get_cache_directory(), 'temp.cbl')
+    with open(tmp_pth, 'w') as f:
+        f.write(code)
+    compiler = GnuCobolCompiler()
+    _, messages = compiler.compile(tmp_pth, get_file_type(tmp_pth))
+    return True, messages
+
+
+class CobolLinterMode(CheckerMode):
+    def __init__(self):
+        super().__init__(lint)
--- open_cobol_ide/view/editors/cobol.py
+++ open_cobol_ide/view/editors/cobol.py
@@ -5,6 +5,7 @@ Contains the cobol code editor widget.
 from pyqode.cobol.widgets import CobolCodeEdit as CodeEditBase
 
 from ...compiler import get_file_type
+from  ...linter import CobolLinterMode
 from ...settings import Settings
 
 
@@ -15,6 +16,11 @@ class CobolCodeEdit(CodeEditBase):
     file type).
 
     """
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.linter_mode = self.modes.append(CobolLinterMode())
+
     @property
     def file_type(self):
         return get_file_type(self.file.path)
