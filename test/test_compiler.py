585282afab75927b74f9492ef9dbc4c1c6fa0588
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2015-05-27 20:10:24 +0200

    Fix tests: default cobc path is now complete on linux

--- test/test_compiler.py
+++ test/test_compiler.py
@@ -106,7 +106,7 @@ def test_make_command_exe(free, std, ftype, expected_opts):
     settings.free_format = free
     settings.cobol_standard = std
     pgm, options = compiler.make_command(['HelloWorld.cbl'], ftype, 'bin')
-    assert pgm == 'cobc'
+    assert pgm == system.which('cobc')
     for o, eo in zip(options, expected_opts):
         assert o == eo
     settings.free_format = free
