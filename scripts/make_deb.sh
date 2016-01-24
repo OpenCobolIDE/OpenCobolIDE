f642e103e0c1dda1f0964b822b21896b36a6f21d
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2016-01-24 22:54:22 +0100

    Add comment to make_deb script

--- scripts/make_deb.sh
+++ scripts/make_deb.sh
@@ -1,7 +1,12 @@
 #! /bin/bash
 
-# install deps: sudo apt-get install python3-stdeb
-
+# This script is meant to be run on Ubuntu 14.04.
+#
+# Execute the following commands to setup your environment:
+#
+# sudo apt-get install python3-pip python3-all debhelper devscripts
+# sudo pip3 install stdeb
+#
 pushd ..
 
 export DEB_BUILD_OPTIONS=nocheck debuild
