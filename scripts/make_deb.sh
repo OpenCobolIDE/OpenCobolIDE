67efd1197da9530aaf90c9408ac5363874489239
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2016-01-24 21:42:58 +0100

    Simplify creation of deb packages

---
+++ scripts/make_deb.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+# install deps: sudo apt-get install python3-stdeb
+
+pushd ..
+
+export DEB_BUILD_OPTIONS=nocheck debuild
+python3 setup.py --command-packages=stdeb.command bdist_deb
