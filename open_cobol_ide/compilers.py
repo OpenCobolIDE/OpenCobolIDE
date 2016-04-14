83b8d9948d0375e06883106cfb69f051775ee82a
Colin Duquesnoy <colin.duquesnoy@gmail.com>
2016-04-14 19:20:27 +0200

    Fix compiler output parser issue
    Use a wildcard for the rest of the error message.
    
    Close #307

--- open_cobol_ide/compilers.py
+++ open_cobol_ide/compilers.py
@@ -165,11 +165,10 @@ class GnuCobolCompiler(QtCore.QObject):
     # GC output messages format depends on the underlying compiler
     # See https://github.com/OpenCobolIDE/OpenCobolIDE/issues/206
     OUTPUT_PATTERN_GCC = re.compile(
-        r'^(?P<filename>[\w\.\-_\s]*):(?P<line>\s*\d*):(?P<type>[\w\s]*):'
-        r'(?P<error>[\w\s,\-\.:\'"\$]*)$')
+        r'^(?P<filename>[\w\.\-_\s]*):(?P<line>\s*\d*):(?P<type>[\w\s]*):(?P<error>.*)$')
     OUTPUT_PATTERN_MSVC = re.compile(
         r'^(?P<filename>[\w\.\-_\s]*)\((?P<line>\s*\d*)\):(?P<type>[\w\s]*):'
-        '(?P<error>[\w\s,\-\.:\'"\$]*)$')
+        '(?P<error>.*)$')
 
     OUTPUT_PATTERNS = [OUTPUT_PATTERN_GCC, OUTPUT_PATTERN_MSVC]
 
